08d3060917f6d203f02a1e3280a2c2433ae626c9
Cameron Custer <custer.cameron@gmail.com>
2021-08-27 12:54:21 -0400

    Hello World

--- Main.java
+++ Main.java
@@ -2,11 +2,8 @@ class Main {
 
 	public static void main(String[] args) {
 
-		int a = 7, b = 2;
-
-		double r = (double) a / b;
-
-		System.out.println(r);
+		System.out.println("Hello, Cameron!");
 
 	}
+
 }
